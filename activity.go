89bedb80bdc6808bc7422a3b46e1839112503dd8
carolina.soares <carolina.soares@litthub.com>
2020-08-31 17:52:30 +0100

    test

--- activity.go
+++ activity.go
@@ -467,7 +467,7 @@ func createAppointment(ctx activity.Context) (email string, success bool) {
 
 	fmt.Println(len(preparationArray))
 
-	if len(preparationArray ) > 0 || preparation == nil {
+	if len(preparationArray ) > 0 || preparation != nil {
 		prepRequest := NewRequest([]string{""}, subject, "")
 		errorPrep := prepRequest.ParseTemplate("template-preparation-iterate.html", data)
 		fmt.Println(errorPrep)
